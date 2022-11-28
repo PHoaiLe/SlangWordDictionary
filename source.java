617259d946a366661ba00d921915596912b293c7
PHoaiLe <lehoaiphuong13042002@gmail.com>
2022-11-28 16:26:13 +0700

    fix bug of search

--- source.java
+++ source.java
@@ -1,12 +1,13 @@
 import java.util.Hashtable;
 import java.util.Vector;
-import java.util.jar.Manifest;
+
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.GridBagConstraints;
-
-
+import javax.swing.JComponent;
+import javax.smartcardio.CommandAPDU;
 import javax.swing.*;
 import java.io.*;
 
@@ -21,8 +22,79 @@ public class source {
 
 class mySlangWordDictionary
 {
-  
+  private Hashtable<String, String> Dictionary;
+  private String SPLIT_SLANGWORD_CHAR = "`";
 
+  mySlangWordDictionary()
+  {
+    Dictionary = new Hashtable<String, String>();
+  }
+
+  public boolean import_Database_fromTXT(String filename)
+  {
+    try
+    {
+        BufferedReader file = new BufferedReader(new FileReader(filename));
+        String aRow = file.readLine();//ignore the first line of instruction;
+        while((aRow = file.readLine()) != null)
+        {
+            String[] splitedData = aRow.split(this.SPLIT_SLANGWORD_CHAR);
+            if(splitedData.length == 2)
+            {
+                this.Dictionary.put(splitedData[0], splitedData[1]);
+            }
+            
+        }
+        return true;
+    }
+    catch(FileNotFoundException f)
+    {
+        return false;
+    }
+    catch(IOException e)
+    {
+        return false;
+    }
+  }
+
+  public String[] searchByKey(String inputKey)
+  {
+    String[] result = new String[2];// result[0]: SlangWord, result[1]: meaning
+    result[1] = this.Dictionary.get(inputKey);
+    if(result[1] == null)
+    {
+        result[0] = null;
+    }
+    else
+    {
+        result[0] = inputKey;
+    }
+    return result;
+  }
+
+  public Hashtable<String, String> searchRelativeKey(String inputKey)
+  {
+    if(inputKey == null || inputKey == "")
+    {
+        return null;
+    }
+    Hashtable<String, String> result = new Hashtable<String, String>();
+    Enumeration<String> keyCollection = this.Dictionary.keys();
+    while(keyCollection.hasMoreElements())
+    {
+        String aKey = keyCollection.nextElement();
+        if(aKey.contains(inputKey))
+        {
+            result.put(aKey, this.Dictionary.get(aKey));
+        }
+    }
+    if(result.size() == 0)
+    {
+        return null;
+    }
+    return result;
+  }
+  
 }
 
 class mainGUI
@@ -35,24 +107,34 @@ class mainGUI
     private QuizMenu quizMenu;
     private CardLayout controlLayout;
 
-    private curOption = 1;
+    private Integer curOption = 1;
     private String sSEARCH = "search";
     private String sRANDOM = "random";
     private String sHISTORY = "history";
     private String sQUIZ = "quiz";
 
+    private mySlangWordDictionary data;
+    private String OriginalDataFile = "./slang.txt";
 
+    private JRadioButton state;
 
     mainGUI()
     {
+
+        this.state = new JRadioButton();
+        state.setVisible(true);
+
+        this.data = new mySlangWordDictionary();
+        data.import_Database_fromTXT(this.OriginalDataFile);
         this.frame = new JFrame();
         controlLayout = new CardLayout(10,10);
         this.mainPanel = new JPanel(controlLayout);
-        this.searchMenu = new SearchMenu();
+        this.searchMenu = new SearchMenu(state);
         this.randomMenu = new RandomMenu();
         this.historyMenu = new HistoryMenu();
         this.quizMenu = new QuizMenu();
 
+        
         JPanel controlPanel = new JPanel(new FlowLayout());
         JButton searchButton = new JButton("Search");
         JButton historyButton = new JButton("History");
@@ -64,7 +146,7 @@ class mainGUI
             public void actionPerformed(ActionEvent ae)
             {
                 switchPanel(1);
-                this.curOption = 1;
+                curOption = 1;
             }
         });
 
@@ -73,7 +155,7 @@ class mainGUI
             public void actionPerformed(ActionEvent ae)
             {
                 switchPanel(2);
-                this.curOption = 2
+                curOption = 2;
             }
         });
 
@@ -82,7 +164,7 @@ class mainGUI
             public void actionPerformed(ActionEvent ae)
             {
                 switchPanel(3);
-                this.curOption = 3;
+                curOption = 3;
             }
         });
 
@@ -91,10 +173,11 @@ class mainGUI
             public void actionPerformed(ActionEvent ae)
             {
                 switchPanel(4);
-                this.curOption = 4;
+                curOption = 4;
             }
         });
 
+        this.frame.add(state, BorderLayout.BEFORE_FIRST_LINE);
         controlPanel.add(searchButton);
         controlPanel.add(historyButton);
         controlPanel.add(randomButton);
@@ -108,7 +191,37 @@ class mainGUI
         mainPanel.add(quizMenu, this.sQUIZ);
         this.frame.add(mainPanel, BorderLayout.CENTER);
 
-
+        state.addItemListener(new ItemListener()
+        {
+            public void itemStateChanged(ItemEvent ie)
+            {
+                if(ie.getStateChange() == 1)
+                {
+                    switch(curOption)
+                    {
+                        case 1:
+                        {
+                            String[] request = searchMenu.getInputText(); //request[0]:inputKey; request[1]:mode
+                            System.out.println("Slang: " + request[0] + " Mode: " + request[1]);
+                            if(request[1].equals("Slang"))
+                            {
+                                String[] res = data.searchByKey(request[0]);
+                                if(res[0] == null)
+                                {
+                                    Hashtable<String, String> relativeWords = data.searchRelativeKey(request[0]);
+                                    searchMenu.setRepresentResult(res[0], res[1], relativeWords);
+                                }
+                                else
+                                {
+                                    searchMenu.setRepresentResult(res[0], res[1], null);
+                                }
+                            }
+                        }break;
+                    }
+                    state.setSelected(false);
+                }
+            }
+        });
     }
 
     public void runGUI()
@@ -171,12 +284,17 @@ class SearchMenu extends JPanel
     private JTextField searchField;
     private String text;
     private Vector<String> RelativeWords;
+    private Vector<String> RelativeMeaning;
     private String curSlagWord;
     private String curDefinition;
+    private JComboBox preRecomList;
+    private JTextArea preSlagText;
+    private JTextArea preDefinitionText;
 
-    SearchMenu()
+    SearchMenu(JRadioButton state)
     {
-        RelativeWords = null;
+        RelativeWords = new Vector<String>();
+        RelativeMeaning = new Vector<String>();
         curDefinition = "";
         curSlagWord = "";
 
@@ -225,22 +343,7 @@ class SearchMenu extends JPanel
         JPanel searchPanel = new JPanel();
         searchField = new JTextField(20);
         JButton searchButton = new JButton("Search");
-        searchButton.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent ae)
-            {
-                String inputedText = searchField.getText();
-                if(inputedText != "" || inputedText != null)
-                {
-                    text = inputedText;
-                }
-                System.out.println(text);
-                System.out.println(selectedMode);
-                preSlagText.setText(curSlagWord);
-                preDefinitionText.setText(curDefinition);
-            }
-        });
-        
+
         searchPanel.add(searchField);
         searchPanel.add(searchButton);
         this.add(searchPanel, BorderLayout.CENTER);
@@ -253,16 +356,35 @@ class SearchMenu extends JPanel
         gbc.insets = new Insets(5,5,5,5);
 
         JLabel preSlagLabel = new JLabel("Slang word:");
-        JTextArea preSlagText = new JTextArea(1, 20);
+        preSlagText = new JTextArea(1, 20);
         JLabel preDefinitionLabel = new JLabel("Definition:");
-        JTextArea preDefinitionText = new JTextArea(10, 20);
+        preDefinitionText = new JTextArea(10, 20);
 
         JLabel preRecomLabel = new JLabel("Recommend:");
-        JList preRecomList = new JList<String>(RelativeWords);
-        preRecomList.setLayoutOrientation(JList.VERTICAL);
+        preRecomList = new JComboBox<String>(RelativeWords);
+        preRecomList.setPreferredSize(new Dimension(100,30));
+        preRecomList.setSelectedIndex(-1);
+        // preRecomList.setLayoutOrientation(JList.VERTICAL);
         JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL);
         scrollBar.add(preRecomList);
         scrollBar.setPreferredSize(new Dimension(100, 100));
+        
+        preRecomList.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ae)
+            {
+                int selectedIndex = preRecomList.getSelectedIndex();
+                System.out.println("Index: " + selectedIndex);
+                if(preRecomList.getSelectedIndex() != -1)
+                {
+                    System.out.println("Re-w: " + RelativeWords.elementAt(selectedIndex));
+                    System.out.println("Re-m: " + RelativeMeaning.elementAt(selectedIndex));
+                    preSlagText.setText(RelativeWords.elementAt(selectedIndex));
+                    preDefinitionText.setText(RelativeMeaning.elementAt(selectedIndex));
+                }
+                
+            }
+        });
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -287,6 +409,23 @@ class SearchMenu extends JPanel
         gbc.gridx = 2;
         gbc.gridy = 1;
         bottom.add(preRecomList, gbc);
+
+        searchButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ae)
+            {
+                String inputedText = searchField.getText();
+                if(inputedText != "" || inputedText != null)
+                {
+                    text = inputedText;
+                }
+                System.out.println(text);
+                System.out.println(selectedMode);
+                preSlagText.setText(curSlagWord);
+                preDefinitionText.setText(curDefinition);
+                state.setSelected(true);
+            }
+        });
         
         this.add(bottom, BorderLayout.PAGE_END);
     }
@@ -299,25 +438,46 @@ class SearchMenu extends JPanel
         return result;
     }
 
-    public void setRepresentResult(String foundSlag, String foundDef, Vector<String> recommend)
+    public void setRepresentResult(String foundSlag, String foundDef, Hashtable<String, String> recommend)
     {
         if(foundSlag == " " || foundSlag == null)
         {
             curSlagWord = "No result";
             curDefinition = " ";
-            RelativeWords.clear();
-            return;
         }
-        curSlagWord = foundSlag;
-        curDefinition = foundDef;
-        RelativeWords.clear();
-        RelativeWords.addAll(recommend);
+        else
+        {
+            curSlagWord = foundSlag;
+            curDefinition = foundDef;
+        }
+        if(recommend != null)
+        {
+            RelativeWords.clear();
+            RelativeMeaning.clear();
+            Enumeration<String> keys = recommend.keys();
+            while(keys.hasMoreElements())
+            {
+                String aKey = keys.nextElement();
+                RelativeWords.addElement(aKey);
+                RelativeMeaning.addElement(recommend.get(aKey));
+            }
+            System.out.println(RelativeWords);
+            System.out.println(RelativeMeaning);
+        }
+        else
+        {
+            RelativeWords.clear();
+            RelativeMeaning.clear();
+        }
+        preSlagText.setText(curSlagWord);
+        preDefinitionText.setText(curDefinition);
+        preRecomList = new JComboBox<String>(RelativeWords);
     }
 }
 
 class HistoryMenu extends JPanel
 {
-
+    private 
 }
 
 class RandomMenu extends JPanel
